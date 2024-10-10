1530c8c36fd4348b7049b9c0668c2f101a38acc9
salviamak <salvia.mak@gmail.com>
2024-10-10 16:52:08 +0200

    component.2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import PokemonCard from "./components/PokemonCard";
 function App() {
   return (
     <div>
-     
+   
       <PokemonCard/>
     </div>
   );
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,30 @@
 // import React from 'react'
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default function PokemonCard() {
+  const pokemon = pokemonList[1];
   return (
     
     <figure>
-      <figcaption>Bulbasaur</figcaption>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+      {pokemon.imgSrc != null ?(
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ):(<p>??????</p>)
+      
+    }
+      <figcaption>
+        {pokemon.name}
+      </figcaption>
     </figure>
   )
 }
+
